884c4612d7e5e5b34c89afe281a4d7e655eb43a8
Giuseppe Spataro <spatyx@gmail.com>
2023-01-05 13:52:53 +0100

    2.1.0

---
+++ assets/js/about.js
@@ -0,0 +1,31 @@
+const categoriesElement = document.getElementById("categories");
+
+document.addEventListener("DOMContentLoaded", () => {
+    const categories = document.querySelectorAll("[data-show]");
+
+    categoriesElement.style.top = headerHeight;
+
+    categories.forEach((anchor) => {
+        anchor.addEventListener("click", () => {
+            document.querySelectorAll(".active[data-show]").forEach((e) => {
+                e.classList.remove("active");
+            });
+
+            document.querySelectorAll("[data-category]").forEach((item) => {
+                item.style.display =
+                    (item.dataset.category == anchor.dataset.show)
+                    ? ""
+                    : "none";
+            });
+
+
+            anchor.classList.add("active");
+        });
+    });
+});
+
+window.addEventListener("resize", () => {
+    setTimeout(() => {
+        categoriesElement.style.top = headerHeight;
+    }, 1);
+});
--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,14 +1,41 @@
-let navbar = document.getElementById("navbar");
-let navbarOverlay = navbar.getElementsByClassName("overlay")[0];
+const header = document.getElementById("header");
+let headerHeight = header.clientHeight + "px";
 
-function uiOpenNavbar()
-{
-    navbar.classList.remove("hidden");
-    navbarOverlay.classList.remove("hidden");
+/**
+ * Fix header placeholder height on resize
+ * @return void
+ */
+
+function headerResizeEvent() {
+    const headerPlaceholder = document.getElementById("header-placeholder");
+    headerHeight = header.clientHeight + "px";
+
+    headerPlaceholder.style.height = headerHeight;
 }
 
-function uiCloseNavbar()
-{
-    navbar.classList.add("hidden");
-    navbarOverlay.classList.remove("hidden");
-}
+document.addEventListener("DOMContentLoaded", () => {
+    /**
+     * Header
+     */
+
+    headerResizeEvent();
+
+    /**
+     * Navbar
+     */
+
+    const navbar = document.getElementById("navbar");
+    const navbarToggle = document.getElementById("navbar-toggle");
+    const navbarToggleOpen = navbarToggle.getElementsByClassName("open")[0];
+    const navbarToggleClose = navbarToggle.getElementsByClassName("close")[0];
+
+    navbarToggle.onclick = function () {
+        document.body.style.overflow = document.body.style.overflow == "hidden" ? "auto" : "hidden";
+        navbarToggleOpen.classList.toggle("hidden");
+        navbarToggleClose.classList.toggle("hidden");
+        navbar.classList.toggle("hidden");
+        navbar.style.top = headerHeight;
+    }
+});
+
+window.addEventListener("resize", headerResizeEvent);
